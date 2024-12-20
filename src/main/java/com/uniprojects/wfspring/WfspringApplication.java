bb962e00a04d8a08eec71b9e914dfe7b448b1d34
Szabados Gergő <gergo.szabados11@gmail.com>
2024-12-20 18:26:04 +0100

    spring starter

---
+++ src/main/java/com/uniprojects/wfspring/WfspringApplication.java
@@ -0,0 +1,13 @@
+package com.uniprojects.wfspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniprojects/wfspring/WfspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniprojects.wfspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
