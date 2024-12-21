296a865c892d6584a80b5cb845de6da233bf6347
Szabados Gergő <gergo.szabados11@gmail.com>
2024-12-21 17:03:29 +0100

    foglalasok component

---
+++ angular/foglalas-angular-app/src/app/foglalasok/foglalasok.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foglalasok',
+  standalone: true,
+  
+  templateUrl: './foglalasok.component.html',
+  styleUrl: './foglalasok.component.css'
+})
+export class FoglalasokComponent {
+
+}
